a29846db83b9cd4c925ed4a832dbfc9f6204ef35
Keukeri <kelian.marchand@icloud.com>
2025-05-10 17:32:27 -0400

    new file:   Image/screen_film.png 	new file:   Image/screen_samiel.png 	modified:   css/css_commun.css 	modified:   css/projet.css 	modified:   css/style.css 	modified:   form.php 	new file:   html/Mentions_legales.html 	deleted:    html/a_propos.html 	deleted:    html/contact.html 	modified:   html/mes_projets.html 	deleted:    html/ok.html 	modified:   index.html 	deleted:    java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let overlay = document.querySelector(".overlay");
-
-    if (overlay) {
-        setTimeout(() => {
-            overlay.classList.add("show"); // Active l'animation
-        }, 500); // Délai avant l'animation
-    }
-});
